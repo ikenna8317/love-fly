ddee40d75c2ce040126376092fadf35b1d0b49d5
ezeajui <ikennaezeaju@gmail.com>
2022-12-28 22:04:35 +0000

    replace the default rect with a triangle to better visualize angular change

--- src/scenes/demo.ts
+++ src/scenes/demo.ts
@@ -11,7 +11,7 @@ const {
 
 export default class Demo extends Phaser.Scene
 {
-    player!: GameObjects.Rectangle
+    player!: GameObjects.Triangle
     cursorKeys: Types.Input.Keyboard.CursorKeys
 
     constructor ()
@@ -23,11 +23,14 @@ export default class Demo extends Phaser.Scene
     {
         //create player and floor placeholders
         const floor: GameObjects.Rectangle = this.add.rectangle(0, CANVAS_HEIGHT - FLOOR_HEIGHT, CANVAS_WIDTH, CANVAS_HEIGHT, 0x4f3d33).setOrigin(0)
-        this.player = this.add.rectangle(PLAYER.spawnX, PLAYER.spawnY, PLAYER.width, PLAYER.height, 0xc72614).setOrigin(0)
+       //this.player = this.add.rectangle(PLAYER.spawnX, PLAYER.spawnY, PLAYER.width, PLAYER.height, 0xc72614).setOrigin(0)
+	this.player = this.add.triangle(PLAYER.spawnX, PLAYER.spawnY, undefined, undefined, undefined, undefined, undefined, undefined, 0xc72614).setOrigin(0)
 
         //enable physics on floor and player objects
         this.physics.add.existing(floor, true)
         this.physics.add.existing(this.player)
+
+	this.player.setData({ isGrounded: false })
 
         if (this.player.body instanceof Phaser.Physics.Arcade.Body) {
             this.player.body.setBounceY(0.1)
@@ -36,7 +39,7 @@ export default class Demo extends Phaser.Scene
             this.player.body.setCollideWorldBounds()
         }
 
-        this.physics.add.collider(this.player, floor)
+        this.physics.add.collider(this.player, floor, () => this.player.setData({ isGrounded: true }))
 
         this.player.setInteractive()
 
@@ -53,8 +56,9 @@ export default class Demo extends Phaser.Scene
                 this.player.body.setVelocityX(PLAYER.speedX)
             
 
-            if (this.cursorKeys.space.isDown) {
+            if (this.cursorKeys.space.isDown && this.player.getData('isGrounded')) {
                 this.player.body.setVelocityY(-1 * PLAYER.jump)
+		this.player.setData({ isGrounded: false })
             }
 
         }
